2df9079bf6c8e5aed9e7cf3dddf0d6e4ce0334ff
dxddyxs <derecklisboaj3@gmail.com>
2026-04-29 19:50:43 -0300

    aula 3 data binding e pipes

--- minha-aplicacao-angular/src/app/app.ts
+++ minha-aplicacao-angular/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Exemplo } from './exemplo/exemplo';
 import { Aula2 } from './aula2/aula2';
+import { Aula3 } from './aula3/aula3';
 
 @Component({
   selector: 'app-root',
-  imports: [Aula2],
+  imports: [Aula3],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- minha-aplicacao-angular/src/app/painel-adm/painel-adm.ts
+++ minha-aplicacao-angular/src/app/painel-adm/painel-adm.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Usuario {
+  id: number;
+  nome: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-painel-adm',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './painel-adm.html',
-  styleUrl: './painel-adm.css',
+  styleUrl: './painel-adm.css'
 })
 export class PainelAdm {
+  usuarios: Usuario[] = [
+    { id: 1, nome: 'Alice', email: 'alice@example.com' },
+    { id: 2, nome: 'Bob', email: 'bob@example.com' },
+    { id: 3, nome: 'Charlie', email: 'charlie@example.com' }
+  ];
 }
