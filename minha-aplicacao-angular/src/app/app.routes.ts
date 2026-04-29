8274e3ab1f33a24bd466c97d84aba90cedebde11
dxddyxs <derecklisboaj3@gmail.com>
2026-04-29 20:08:28 -0300

    aula 6 guards

--- minha-aplicacao-angular/src/app/app.routes.ts
+++ minha-aplicacao-angular/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {
@@ -27,10 +28,10 @@ export const routes: Routes = [
             import('./painel-adm/painel-adm').then(
                 (m) => m.PainelAdm
             ),
+        canActivate: [authGuard],
     },
     {
         path: '**',
         redirectTo: 'produtos',
     }
 ];
-
